d8c799a67ef9c0b3f034ff26c5a017dfe2b65ed6
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-12-02 03:06:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "● Tʜᴀɴᴋ Yᴏᴜ Fᴏʀ Uꜱɪɴɢ Oᴜʀ Bᴏᴛ ❤️ \n\n● Tᴏ Gᴇᴛ Aʟʟ ʟᴀᴛᴇꜱᴛ Uᴘᴅᴀᴛᴇꜱ Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ 🔥\n\n ● Lɪɴᴋ :~ Tᴇᴀᴍ Lᴇɢᴇɴᴅ Oꜰꜰɪᴄɪᴀʟ (https://t.me/Team_Legend_Official) ⚡️"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
